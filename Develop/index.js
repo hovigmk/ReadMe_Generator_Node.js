1f3e528ed0093f06dfb7b3b533f93c7841655a12
Hovig Kalayjian <hovigkalayjian@gmail.com>
2022-12-13 19:36:29 -0500

    gathered the links and badges

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,73 @@
-// TODO: Include packages needed for this application
+// Including packages needed for this application
+
+const fs = require("fs");
+
+const inquirer = require("inquirer");
+
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {}
+
+// function to generate the ReadMe here
+// function writeToFile(fileName, data) {
+//   fs.writeFile(fileName, generateMarkdown(data), function (err) {
+//     if (err) {
+//       return console.log(err);
+//     }
+//   });
+// }
+
+// fs.writeFile(fileName, generateMarkdown(data), (err) =>
+//   err ? console.error(err) : console.log('Success!')
+// );
+
+// function to initalize the beginning of the questions
+
+// inquirer
+//   .prompt([
+//     {
+//       type: 'input',
+//       message: 'What is your user name?',
+//       name: 'username',
+//     },
+//     {
+//       type: 'password',
+//       message: 'What is your password?',
+//       name: 'password',
+//     },
+//     {
+//       type: 'password',
+//       message: 'Re-enter password to confirm:',
+//       name: 'confirm',
+//     },
+//   ])
+//   .then((response) =>
+//     response.confirm === response.password
+//       ? console.log('Success!')
+//       : console.log('You forgot your password already?!')
+//   );
+
+function init() {
+  inquirer.prompt(questions).then((data) => {
+    console.log(JSON.stringify(data, null, " "));
+    data.getLicense = getLicense(data.license);
+    writeToFile("./example/README.md", data);
+  });
+}
+
+//  const init = () => {
+//  inquirer.prompt(questions).then((data) => {
+//     console.log(JSON.stringify(data, null, " "));
+//     data.getLicense = getLicense(data.license);
+//     writeToFile("./example/README.md", data);
+//   });
+// }
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -18,3 +18,12 @@ function generateMarkdown(data) {
 }
 
 module.exports = generateMarkdown;
+
+// GNU AGPLv3  return "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)";
+// GNU GPLv3 return [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+// GNU LGPLv3  return "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)";
+// Mozilla Public License 2.0 return [![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0);
+// Apache License 2.0   return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+// MIT License  return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+// Boost Software License 1.0   return "[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
+// The Unlicense return [![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/);
